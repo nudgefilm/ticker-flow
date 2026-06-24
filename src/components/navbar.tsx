cfbd7f8ad1a161168f6cb3a3ee17cedc9b7d18d8
nudgefilm <nudgefilm@gmail.com>
2026-06-24 17:38:37 +0900

    fix: restore Link import in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import Logo from "@/components/logo";
 import { cn } from "@/lib/utils";
